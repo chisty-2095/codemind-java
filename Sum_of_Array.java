e6461e61c31bdd1deddefded9a614aea0e873d2d
chisty-2095 <119730182+chisty-2095@users.noreply.github.com>
2023-08-19 11:49:47 +0530

    import java.util.Scanner; public class soa {     public static void main(String[] args) {         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         int[] intArray = new int[n];         for (int i = 0; i < n; i++) {             intArray[i] = sc.nextInt();         }         int sum = 0;         for (int elem: intArray) {             sum += elem;         }         System.out.println(sum);     } }

---
+++ Sum_of_Array.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class soa {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] intArray = new int[n];
+        for (int i = 0; i < n; i++) {
+            intArray[i] = sc.nextInt();
+        }
+        int sum = 0;
+        for (int elem: intArray) {
+            sum += elem;
+        }
+        System.out.println(sum);
+    }
+}
