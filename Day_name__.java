1f8b83b8d1ca9d72467be2d14a9cf994e64cdda5
chisty-2095 <119730182+chisty-2095@users.noreply.github.com>
2023-08-19 12:21:27 +0530

    import java.util.Scanner; public class day {     public static void main(String[] args) {         Scanner sc = new Scanner(System.in);         int n;         n = sc.nextInt();         if (n == 1) {             System.out.print("Monday");         }         else if (n == 2) {             System.out.print("Tuesday");         }         else if (n == 3) {             System.out.print("Wednesday");         }         else if (n == 4) {             System.out.print("Thursday");         }         else if (n == 5) {             System.out.print("Friday");         }         else if (n == 6) {             System.out.print("Saturday");         }         else if (n == 7) {             System.out.print("Sunday");         }         else {             System.out.print("Invalid input");         }     } }

---
+++ Day_name__.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class day {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+        if (n == 1) {
+            System.out.print("Monday");
+        }
+        else if (n == 2) {
+            System.out.print("Tuesday");
+        }
+        else if (n == 3) {
+            System.out.print("Wednesday");
+        }
+        else if (n == 4) {
+            System.out.print("Thursday");
+        }
+        else if (n == 5) {
+            System.out.print("Friday");
+        }
+        else if (n == 6) {
+            System.out.print("Saturday");
+        }
+        else if (n == 7) {
+            System.out.print("Sunday");
+        }
+        else {
+            System.out.print("Invalid input");
+        }
+    }
+}
