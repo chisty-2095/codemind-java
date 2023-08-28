09d959583382bb4214e6c906080c14b276a35494
chisty-2095 <119730182+chisty-2095@users.noreply.github.com>
2023-08-28 22:32:20 +0530

    Add files via upload

---
+++ brke.java
@@ -0,0 +1,27 @@
+// public class brke {
+//     public static void main(String[] args) {
+//         for (int i = 100; i >= 1; i--) {
+//             System.out.print(i + " ");
+//             if (i % 2 == 1) {
+//                 break;
+//             }
+//         }
+//     }
+// }
+
+
+// public class brke {
+//     public static void main(String[] args) {
+//         int i = 1;
+//         while (true) {
+//             System.out.print(i + " ");
+//             if (i == 10) {
+//                 break;
+//             }
+//             i++;
+//         }
+//     }
+// }
+
+
+public class 
