7d1b759ce999b349c2c7d4e401f3eee527805d72
chisty-2095 <119730182+chisty-2095@users.noreply.github.com>
2023-08-28 14:21:39 +0530

    import java.util.Scanner; public class aoc {     public static void main(String[] args) {         Scanner sc = new Scanner(System.in);         double r;         r = sc.nextDouble();         double a;         a = 3.14*r*r;         System.out.printf("%.2f",a);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class aoc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double r;
+        r = sc.nextDouble();
+        double a;
+        a = 3.14*r*r;
+        System.out.printf("%.2f",a);
+    }
+}
