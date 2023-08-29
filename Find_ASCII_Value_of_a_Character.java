49fbad4b146748aad22e25246bf08f646c127482
chisty-2095 <119730182+chisty-2095@users.noreply.github.com>
2023-08-29 22:43:49 +0530

    import java.util.Scanner; public class favc {     public static void main(String[] args) {         Scanner sc = new Scanner(System.in);         char ch = sc.next().charAt(0);         int c = (int) ch;         System.out.println(c);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class favc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        int c = (int) ch;
+        System.out.println(c);
+    }
+}
