6a8a9a2052301fbd7d5a24d22f33230f8eaeb961
chisty-2095 <119730182+chisty-2095@users.noreply.github.com>
2023-09-01 22:45:56 +0530

    Add files via upload

---
+++ nesw.java
@@ -0,0 +1,11 @@
+import java.util.scanner;
+public class nesw{
+    public static void main(String[] args){
+        int n,m;
+        Scanner sc = new Scanner(System.in)
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        switch()
+    }
+}
+
