3575374a1a841899310d7a8ead02e7f513a69a86
chisty-2095 <119730182+chisty-2095@users.noreply.github.com>
2023-09-01 22:19:44 +0530

    import java.util.Scanner; public class tlc {     public static void main(String[] args) {         Scanner sc = new Scanner(System.in);         String s = sc.nextLine();         String s2 = s.toLowerCase();         System.out.println(s2);     } }

---
+++ To_Lower_Case.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class tlc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String s2 = s.toLowerCase();
+        System.out.println(s2);
+    }
+}
