8e7660636477b0089b6b2b4ca82f4fb3f6620f6d
chisty-2095 <119730182+chisty-2095@users.noreply.github.com>
2023-09-02 22:03:38 +0530

    Add files via upload

---
+++ moin.java
@@ -0,0 +1,17 @@
+public class moin {
+    public static void main(String[] args)
+    {
+        int n=10;
+        switch(n){
+            case 1:
+            System.out.println("One");
+            break;
+            case 2:
+            System.out.println("Two");
+            break;
+            default:
+            System.out.println("Invalid Choice");
+        }
+    }
+    
+}
