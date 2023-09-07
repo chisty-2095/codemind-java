a48d89117596597a6d1fc6d64175a01d9b00b5d5
chisty-2095 <119730182+chisty-2095@users.noreply.github.com>
2023-09-07 11:28:03 +0530

    import java.util.Scanner; public class ht {     public static void main(String[] args) {         Scanner sc = new Scanner(System.in);         String s = "Hello Technicalhub";         String t = sc.nextLine();         System.out.println(s);         System.out.print(t);     } }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class ht {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = "Hello Technicalhub";
+        String t = sc.nextLine();
+        System.out.println(s);
+        System.out.print(t);
+    }
+}
