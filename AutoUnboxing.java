18d28e43913299ad817f8397832081943738dc3b
chisty-2095 <119730182+chisty-2095@users.noreply.github.com>
2023-09-13 22:29:36 +0530

    AutoUnboxing.java

---
+++ AutoUnboxing.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+class Unboxing {
+	public static void main(String[] args)
+	{
+		Character ch = 'a';
+		char a = ch;
+		ArrayList<Integer> arrayList = new ArrayList<Integer>();
+		arrayList.add(24);
+		int num = arrayList.get(0);
+		System.out.println(num);
+	}
+}
