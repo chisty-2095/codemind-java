bad826039a9b4c12311f722974828927067d4a4b
chisty-2095 <119730182+chisty-2095@users.noreply.github.com>
2023-09-30 14:26:45 +0530

    Double_to_String.java

---
+++ Double_to_String.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main(String[] args)
+	{
+		double number = 123.456;
+		String output = String.valueOf(number);
+		System.out.println(output);
+	}
+}
